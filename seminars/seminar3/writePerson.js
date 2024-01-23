8c9bea377f559e3a128951a949e5462ef24c53ed
Nord.A <mr.nordakov@gmail.com>
2024-01-23 14:44:03 +0300

    seminar3.1

--- seminars/seminar3/writePerson.js
+++ seminars/seminar3/writePerson.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const path = require("path");
 const { json } = require("stream/consumers");
 
-const Person = {
+const person = {
   name: "Ivan",
   surname: "Ivanov",
   age: 30,
-  city: "Moscow",
+  city: "SPB",
 };
 
-const personJson = JSON.stringify(Person, null, 2);
+const personJson = JSON.stringify(person, null, 2);
 const pathToFile = path.join(__dirname, "person.json");
 
 fs.writeFileSync(pathToFile, personJson);
